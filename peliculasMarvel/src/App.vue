a62f942991888ecd19caf3f3c5506c22707a547f
PabloPDV <you@example.com>
2024-08-23 10:45:24 +0200

    modificacion App.vue

--- peliculasMarvel/src/App.vue
+++ peliculasMarvel/src/App.vue
@@ -1,20 +1,9 @@
 <template>
   <div id="app">
-    <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </header>
-    <RouterView />
+    <ListaPeliculas />
   </div>
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
+import ListaPeliculas from './components/ListaPeliculas.vue';
 </script>
-
-<style scoped>
-/* Tus estilos aquí */
-</style>
